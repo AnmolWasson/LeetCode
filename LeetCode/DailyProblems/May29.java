88f289e2086c1818635051b737b6358f678106cf
AnmolWasson <Anmolwassan55@gmail.com>
2022-05-29 15:17:29 +0530

    LeetCode

---
+++ LeetCode/DailyProblems/May29.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class May29 {
+
+  //318. Maximum Product of Word Lengths
+  public int maxProduct(String[] words) {
+    int n = words.length;
+    
+    
+    
+    int[][] freq = new int[n][26];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < words[i].length(); j++) {
+        char ch = words[i].charAt(j);
+        freq[i][ch - 'a'] = 1;
+      }
+    }
+
+    int max = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        int count = 0;
+        for (int k = 0; k < 26; k++) {
+          count += (freq[i][k] & freq[j][k]);
+        }
+
+        if (count == 0) {
+          max = Math.max(max, words[i].length() * words[j].length());
+        }
+      }
+    }
+
+    return max;
+  }
+}
